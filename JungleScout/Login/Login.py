63df172987783ff1aef67571e4a6f93aaa8410c6
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2023-06-19 18:00:33 +0100

    still download button for the chrome extension

--- JungleScout/Login/Login.py
+++ JungleScout/Login/Login.py
@@ -11,8 +11,8 @@ import pytesseract
 from PIL import Image
 
 options = webdriver.ChromeOptions()
-options.add_extension('/home/keranis/work&study/Cocobot-jungle-scout-Amazon-scrapping/JungleScout/Login/js.crx')
-options.add_argument('--user-data-dir=/home/keranis/.config/google-chrome/Default')
+options.add_argument("--user-data-dir=/home/keranis/.config/google-chrome/Default")
+options.add_argument("--load-extension=/home/keranis/.config/google-chrome/Default/Extensions/bckjlihkmgolmgkchbpiponapgjenaoa/7.4.0_0")
 class Login(uc.Chrome):
     def __init__(self, teardown=False):
         self.teardown = teardown
@@ -28,10 +28,46 @@ class Login(uc.Chrome):
             input('Press Any key to close the program and press enter')
 
     def land_first_page(self):
-        self.implicitly_wait(50)
-        pass
-        # self.get(const.jungle)
-        # self.implicitly_wait(100)
+        # query = input("What would you search ?")
+        query = "I phone 10"
+        self.get(const.amazon + f"/s?k={query.replace(' ', '+')}")
+        self.implicitly_wait(100)
+        self.switch_to.window(self.window_handles[1])
+        self.close()
+        self.switch_to.window(self.window_handles[0])
+        self.implicitly_wait(100)
+        time.sleep(5)
+        try :
+            # jsBtn = WebDriverWait(self, 100).until(
+            #     EC.presence_of_element_located((By.ID, "popup-button"))
+            # )
+            jsBtn = self.find_element(By.ID,"popup-button")
+            jsBtn.click()
+            time.sleep(5)
+            stillRecherche = True
+            while(stillRecherche):
+                try:
+                    jsBtn = self.find_element(By.CSS_SELECTOR,"#jsExtensionBaseModalId > div.Container-sc-1tzcbkm-1.gNfIMq > div > div.Flex-sc-sqmtka-0.TableContainer-sc-ybu0mp-1.bXUgal.hhoMvF > div.Flex-sc-sqmtka-0.Container-sc-n6pzpt-0.eQnaRt.huoEMs > div:nth-child(2) > div.Flex-sc-sqmtka-0.ftsCjc > button")
+                    jsBtn.click()
+                    time.sleep(5)
+                    updatedContent = self.find_element(By.CSS_SELECTOR,"#jsExtensionBaseModalId > div.Container-sc-1tzcbkm-1.gNfIMq > div > div.Flex-sc-sqmtka-0.TableContainer-sc-ybu0mp-1.bXUgal.hhoMvF > div.Flex-sc-sqmtka-0.Container-sc-n6pzpt-0.eQnaRt.huoEMs > div:nth-child(2) > div.Flex-sc-sqmtka-0.ftsCjc > button > div > div")
+                    print("->", updatedContent.text)
+                    if updatedContent.text != "Charger plus de résultats" and updatedContent.text != "Loading...":
+                        stillRecherche = False
+                    elif updatedContent.text == "Charger plus de résultats" :
+                        stillRecherche = True
+                except Exception as ec:
+                    stillRecherche = False
+            jsBtn = self.find_element(By.CSS_SELECTOR,"#jsExtensionBaseModalId > div.Container-sc-1tzcbkm-1.gNfIMq > div > div.Flex-sc-sqmtka-0.TableContainer-sc-ybu0mp-1.bXUgal.hhoMvF > div.Flex-sc-sqmtka-0.Container-sc-n6pzpt-0.eQnaRt.huoEMs > div:nth-child(1) > div.Flex-sc-sqmtka-0.kGVKPL > div > div > div")
+            print("we'll start downloading")
+            jsBtn.click()
+            time.sleep(5)
+            jsBtn = self.find_element(By.CSS_SELECTOR,"#radix-\:r22e\: > div > div:nth-child(1)")
+            print("download")
+            jsBtn.click()
+        except ZeroDivisionError:
+            print("Jungle Scout Exception please contact your administrator!")
+
         # time.sleep(5)
         # email = self.find_element(
         #     By.ID, "email"
--- JungleScout/test.py
+++ JungleScout/test.py
@@ -6,10 +6,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 if __name__ == '__main__':
     options = webdriver.ChromeOptions()
-    options.add_argument("--user-data-dir=C:\\Users\\mahdi\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
-    options.add_argument("--load-extension=C:\\Users\\mahdi\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\bckjlihkmgolmgkchbpiponapgjenaoa\\7.4.0_0")
-    # options.add_extension("C:\\Users\\mahdi\\Desktop\\don't\\Cocobot-jungle-scout-Amazon-scrapping\\Extension\\js.crx")
+    options.add_argument("--user-data-dir=/home/keranis/.config/google-chrome/Default")
+    options.add_argument("--load-extension=/home/keranis/.config/google-chrome/Default/Extensions/bckjlihkmgolmgkchbpiponapgjenaoa/7.4.0_0")
+    # options.add_argument("--user-data-dir=C:\\Users\\mahdi\\AppData\\Local\\Google\\Chrome\\User Data\\Default")
+    # options.add_argument("--load-extension=C:\\Users\\mahdi\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\bckjlihkmgolmgkchbpiponapgjenaoa\\7.4.0_0")
     browser = uc.Chrome(options=options)
     browser.get("https://www.google.com")
     browser.maximize_window()
+    browser.switch_to.window(browser.window_handles[1])
+    browser.close()
     input("press enter")
